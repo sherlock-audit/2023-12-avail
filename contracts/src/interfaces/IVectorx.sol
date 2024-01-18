81f99311320f1e4b402fbbd2db8f7d4cba1d4b6b
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2024-01-18 10:49:31 +0100

    Delete scope directories

--- contracts/src/interfaces/IVectorx.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.23;
-
-interface IVectorx {
-    function dataRootCommitments(bytes32 rangeHash) external view returns (bytes32 dataRoot);
-}
